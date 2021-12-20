7d4501ec22389352fc6cc82ca3a33b7c4803fc39
zept01 <pascal.bauer-1@gmx.net>
2021-12-20 22:01:49 +0100

    Update POM & Add JSP support

--- src/main/java/com/springboot/web/springbootwebapplication/controller/LoginController.java
+++ src/main/java/com/springboot/web/springbootwebapplication/controller/LoginController.java
@@ -2,17 +2,12 @@ package com.springboot.web.springbootwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-// /login -> "Hello World!"
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login")
-    @ResponseBody
+    @RequestMapping("/login")
     public String loginMessage() {
-        return "Hello World!";
+        return "login";
     }
-
-
 }
