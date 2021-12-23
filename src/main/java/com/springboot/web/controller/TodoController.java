e31d4fd4a52c4a6f9ffa8ce6977dc31394c9d229
zept01 <pascal.bauer-1@gmx.net>
2021-12-23 23:45:03 +0100

    You can add todos now!

--- src/main/java/com/springboot/web/controller/TodoController.java
+++ src/main/java/com/springboot/web/controller/TodoController.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Date;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
     @Autowired
@@ -15,10 +20,20 @@ public class TodoController {
 
     @RequestMapping(value = "/list-todos", method = RequestMethod.GET)
     public String showTodos(ModelMap modelMap) {
-
-        modelMap.put("todos", todoService.retrieveTodos("Pascal"));
-        
+        String name = (String) modelMap.get("name");
+        modelMap.put("todos", todoService.retrieveTodos(name));
         return "list-todos";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+    public String showAddTodoPage(ModelMap modelMap) {
+        return "todo";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+    public String addTodo(ModelMap modelMap, @RequestParam String desc) {
+        todoService.addTodo((String) modelMap.get("name"), desc, new Date(), false);
+        return "redirect:/list-todos";
     }
 
 
--- src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -5,8 +5,10 @@
 </head>
 
 <body>
-<p>Here is the list of your todos:</p>
+<p>Here is the list of ${name}'s todos:</p>
 ${todos}
+<br/>
+<a href="/add-todo">Add a Todo</a>
 </body>
 
 </html>
